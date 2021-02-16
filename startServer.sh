246ca20e149dd480e894e148c63bac62fd75749d
Cristian Paul Bara <cpbara@umich.edu>
2021-02-16 08:34:40 -0500

    updated start script

--- startServer.sh
+++ startServer.sh
@@ -3,7 +3,8 @@
 
 # Generate Plan
 printf "\nGenerating plan..."
-python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=1 --lower_complex_lim=1 --num_tools=2
+python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=2 --disparate_knowledge
+# python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=1 --lower_complex_lim=1 --num_tools=2
 printf "Done!\n"
 
 # Start Server
