4bb364591955198bce5afae200ab8b76434473f8
Cristian Paul Bara <cpbara@umich.edu>
2021-03-17 13:18:12 -0400

    logs are ided with server ip. db drpped after game

--- startServer.sh
+++ startServer.sh
@@ -3,19 +3,33 @@
 
 # Spigot Logs
 mkdir -p logs
-current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-log_file="logs/logs.${current_time}.txt"
-plan_file="logs/logs.${current_time}.plan.json"
+my_ip=$(ifconfig  | grep 'inet '| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $2}' | tr '.' '_')
+current_time="${my_ip}_$(date "+%Y%m%d_%H%M%S")"
+
+mkdir -p "logs/${current_time}"
+
+echo $current_time > log_dest.tmp
+
+tmp=$(cat log_dest.tmp)
+
+echo "${tmp}.log"
+
+log_file="logs/${current_time}/mcc_${current_time}.log"
+plan_file="logs/${current_time}/plan_${current_time}.json"
 touch $log_file
 printf "\nLog file created ${log_file}" | tee -a $log_file
+
+# printf "\n"
+# exit 1
 
 # Generate Plan
 
 printf "\nGenerating plan..."
 # python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=2 --disparate_knowledge
 # python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=2
-python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=1 --disparate_knowledge
-# python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=1
+# python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=1 --disparate_knowledge
+python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=1
+
 cp spigot/plan.json $plan_file
 cp spigot/plan.json mean/plan.json
 printf "Done!\n"
@@ -25,4 +39,6 @@ printf "Done!\n"
 printf "\nStarting Server...\n\n" | tee -a $log_file
 bash spigot/start.command 2>&1 | tee -a $log_file
 
-mysqldump --add-drop-table -u root --password minecraft > "logs/mcc_${current_time}.sql"
+mysqldump --add-drop-table -u root --password minecraft > "logs/${current_time}/mcc_${current_time}.sql"
+
+echo "DROP DATABASE minecraft; CREATE DATABASE minecraft;" | mysql -u root -p
