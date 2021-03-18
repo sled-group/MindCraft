027dece17578c029a11c7217598e468a78751051
Cristian Paul Bara <cpbara@umich.edu>
2021-03-18 12:25:56 -0400

    game order for Sky

--- startServer.sh
+++ startServer.sh
@@ -25,10 +25,10 @@ printf "\nLog file created ${log_file}" | tee -a $log_file
 # Generate Plan
 
 printf "\nGenerating plan..."
+python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=1                       # SK-SS
+# python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=2 --disparate_knowledge # DK-DS
 # python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=2                       # SK-DS
 # python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=1 --disparate_knowledge # DK-SS
-# python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=1                       # SK-SS
-python3 plan_generator/plan_generator.py --num_final_mat=1 --output_path=spigot/plan.json --upper_complex_lim=2 --lower_complex_lim=2 --num_tools=2 --disparate_knowledge # DK-DS
 
 cp spigot/plan.json $plan_file
 cp spigot/plan.json mean/plan.json
